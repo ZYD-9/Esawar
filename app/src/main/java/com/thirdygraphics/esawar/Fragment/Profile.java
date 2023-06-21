5ce7f27ba044acc6721adc7d45dd2f14af2a2409
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-06-22 07:57:37 +0800

    update color system design

--- app/src/main/java/com/thirdygraphics/esawar/Fragment/Profile.java
+++ app/src/main/java/com/thirdygraphics/esawar/Fragment/Profile.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.joanzapata.iconify.Iconify;
 import com.thirdygraphics.esawar.R;
 
 public class Profile extends Fragment {
