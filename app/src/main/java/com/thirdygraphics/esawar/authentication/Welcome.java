c5bf2f3aa799c61593fc79775137ef6d11693188
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-06-22 10:09:51 +0800

    add Welcome Page with intent

--- app/src/main/java/com/thirdygraphics/esawar/authentication/Welcome.java
+++ app/src/main/java/com/thirdygraphics/esawar/authentication/Welcome.java
@@ -2,8 +2,11 @@ package com.thirdygraphics.esawar.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
 import com.thirdygraphics.esawar.R;
 
 public class Welcome extends AppCompatActivity {
@@ -12,5 +15,29 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+        // Find the SignIn button by its id
+        MaterialButton btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the SignInActivity
+                Intent intent = new Intent(Welcome.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the CreateAccount button by its id
+        MaterialButton btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the CreateAccountActivity
+                Intent intent = new Intent(Welcome.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
