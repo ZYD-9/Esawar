9331972bf44576960aaab664ec51bfb38e72e5f0
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-06-22 18:48:06 +0800

    Login Integration 80%

--- app/src/main/java/com/thirdygraphics/esawar/authentication/Login.java
+++ app/src/main/java/com/thirdygraphics/esawar/authentication/Login.java
@@ -1,17 +1,216 @@
 package com.thirdygraphics.esawar.authentication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.thirdygraphics.esawar.MainActivity;
 import com.thirdygraphics.esawar.R;
 
-
 public class Login extends AppCompatActivity {
+    private EditText edtEmail, editPassword;
+    private Button  btnLogin;
+    private LinearLayout btnGoogle, btnFacebook;
+    private  TextView btnForgotPassword;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize views
+        edtEmail = findViewById(R.id.edtEmail);
+        editPassword = findViewById(R.id.editPassword);
+        btnForgotPassword = findViewById(R.id.btnForgotPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        btnFacebook = findViewById(R.id.btnFacebook);
+
+        // Set click listener for btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform error handling for edtEmail and editPassword
+                String email = edtEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    edtEmail.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    editPassword.setError("Password is required");
+                    return;
+                }
+
+                // Sign in with Firebase Authentication
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Successfully signed in
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // Check the role in Firestore
+                                    checkUserRole(user.getUid());
+                                } else {
+                                    // Sign in failed
+                                    Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        // Set click listener for btnGoogle
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sign in with Google
+                signInWithGoogle();
+            }
+        });
+
+        // Set click listener for btnFacebook (Coming Soon)
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this, "Coming Soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set click listener for btnForgotPassword (Coming Soon)
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this, "Coming Soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Function to check the role in Firestore
+    // Function to check the role in Firestore
+    private void checkUserRole(String userId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference userRef = db.collection("Users").document(userId);
+
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String role = document.getString("role");
+
+                        if (role != null && role.equals("user")) {
+                            // Role is "users", intent to MainActivity
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Invalid user
+
+                            Toast.makeText(Login.this, "Invalid user", Toast.LENGTH_SHORT).show();
+                            mAuth.signOut();
+                        }
+                    } else {
+                        // Invalid user
+                        Toast.makeText(Login.this, "User Not Existed! Contact Admin", Toast.LENGTH_SHORT).show();
+                        mAuth.signOut();
+                    }
+                } else {
+                    // Error occurred while fetching the document
+                    Toast.makeText(Login.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    // Function to sign in with Google
+    private void signInWithGoogle() {
+        // Configure Google Sign-In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Get the signInIntent and start the activity for result
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed
+                Toast.makeText(Login.this, "Google Sign In failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Function to authenticate with Firebase using Google credentials
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Successfully signed in
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            // Check the role in Firestore
+                            checkUserRole(user.getUid());
+                        } else {
+                            // Sign in failed
+                            Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
